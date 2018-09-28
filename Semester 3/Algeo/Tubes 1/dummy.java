2554f95dc8910bb3bd325adf21bc86d9d67ada13
irfansofyana <irfansofyana0305@gmail.com>
2018-09-28 08:35:24 +0700

    add an interpolation

--- Semester 3/Algeo/Tubes 1/dummy.java
+++ Semester 3/Algeo/Tubes 1/dummy.java
@@ -44,7 +44,7 @@ class Matriks{
 }
 
 class Tubes{
-	public static void EliminasiGauss(Matriks M){
+	public static void EliminasiGauss(Matriks M, double[] solusi){
 		int batasBaris = M.persamaan;
 		int batasKolom = M.variabel+1;
 		int baris = 1;
@@ -83,12 +83,12 @@ class Tubes{
 		}
 		if (isSolutionExist) {
 			M.tulisMatriks(M);
-			GaussJordan(M);
+			GaussJordan(M, solusi);
 		}else{
 			System.out.println("No solution exist for that equations!");
 		}
 	}
-	public static void GaussJordan(Matriks M){
+	public static void GaussJordan(Matriks M,double[] solusi){
 		int batasBaris = M.persamaan;
 		int batasKolom = M.variabel+1;
 		int baris = 1;
@@ -122,25 +122,123 @@ class Tubes{
 		}
 		System.out.println("Matriks RREFnya adalah ");
 		M.tulisMatriks(M);
-		for (int i = 1; i <= batasBaris; i++){
-			int j = 1;
-			while (j <= batasKolom && M.mat[i][j] == 0) j++;
-			if (j > batasKolom) {
-				System.out.println("X["+i+"] = X["+i+"]");
-				continue;
+		int rowzero = 0;
+		for (int i = batasBaris; i >= 1; i--){
+			if (M.isAllzero(M,i)) rowzero++;
+		}
+		if (batasBaris-rowzero == batasKolom-1) {
+			for (int i = 1; i <= batasBaris; i++){
+					solusi[i] = M.mat[i][batasKolom];
 			}
-			System.out.print("X["+i+"] = ");
-			for (int k = j+1; k <= batasKolom-1; k++){
-				if (M.mat[i][k] != 0) {
-					System.out.print(-1*M.mat[i][k] + "*X["+k+"] + ");
-				} 
+		}else{
+			boolean[] sudah = new boolean[105];
+			for (int i = 1; i <= batasKolom-1; i++)
+				sudah[i] = false;
+			for (int i = 1; i <= batasBaris; i++){
+					int j = 1;
+					while (j < batasKolom && M.mat[i][j] == 0) j++;
+					if (j >= batasKolom || i > batasBaris) {
+						// System.out.println("X["+i+"] = X["+i+"]");
+						// cek[i] = false;
+						continue;
+					}
+					sudah[j] = true;
+					System.out.print("X["+j+"] = ");
+					boolean pertamaOutput = true;
+					for (int k = j+1; k <= batasKolom-1; k++){
+						if (M.mat[i][k] != 0) {
+							if (pertamaOutput){
+								System.out.print("("+ -1*M.mat[i][k] +")"+ "*X["+k+"]");
+								pertamaOutput = false;
+							}else{
+								System.out.print(" + "+"("+ -1*M.mat[i][k] +")"+ "*X["+k+"]");
+							}
+						} 
+					}
+					if (M.mat[i][batasKolom] != 0 && !pertamaOutput) System.out.println(" + ("+ -1*M.mat[i][batasKolom] +")");
+					else if (M.mat[i][batasKolom] != 0) System.out.println(M.mat[i][batasKolom]);
+					else System.out.println();
 			}
-			System.out.println(M.mat[i][batasKolom]);
+			for (int i = 1; i <= batasKolom-1; i++){
+				if (!sudah[i]){
+					System.out.println("X["+i+"] = X["+i+"]");
+				}
+			}
+
 		}
 	}
+	public static double hitungFungsi(double[] solusi, int derajat, double x){
+		double res = 0;
+		for (int i = 1; i <= derajat+1; i++){
+			res += Math.pow(x, i-1)*solusi[i];
+		}
+		return res;
+	}
+
+	public static void interpolasi(){
+		/* KAMUS */
+		Matriks t = new Matriks();
+		Matriks spl = new Matriks();
+		double xtaksir;
+		double[] solusi = new double[105];
+		// Tubes tbs = new Tubes();
+
+		/* ALGORITMA */
+		/* Input derajat interpolasi */
+		Scanner in = new Scanner(System.in);
+		System.out.print("Derajat interpolasi : ");
+		int n = in.nextInt();
+
+		/* Input banyak titik */
+		System.out.println("Input titik :");
+		for(int i = 1; i <= n + 1; i++) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			t.mat[i][1] = x;
+			t.mat[i][2] = y;
+		}
+
+		System.out.print("Nilai yang akan ditaksir : ");
+		xtaksir = in.nextDouble();
+		/* Pindahkan ke dalam bentuk SPL */
+		for(int i = 1; i <= n + 1; i++) {
+			for(int j = 1; j <= n + 2; j++) {
+				if(j == 1) {
+					spl.mat[i][j] = 1;
+				} else if(j == n + 2) {
+					spl.mat[i][j] = t.mat[i][2];
+				} else {
+					spl.mat[i][j] = Math.pow(t.mat[i][1], j - 1);
+				}
+			}
+		}
+		spl.persamaan = n+1;
+		spl.variabel = n+1;
+
+		/* Tulis matriks SPL */
+		for(int i = 1; i <= n + 1; i++) {
+			for(int j = 1; j <= n + 2; j++) {
+				System.out.printf("%f", spl.mat[i][j]);
+				if(j != n + 2) {
+					System.out.print(" ");
+				} else if(i != n + 1) {
+					System.out.println();
+				}
+			}
+		}
+		System.out.println();
+		EliminasiGauss(spl, solusi);
+		System.out.println("Maka nilai taksiran fungsi pada x = "+ xtaksir + " adalah " + hitungFungsi(solusi, n, xtaksir));
+	}
+
 	public static void main(String[] args){
 		Matriks M = new Matriks();
+		double[] solusi = new double[1005];
 		M.inputMatriks(M);
-		EliminasiGauss(M);
+		EliminasiGauss(M, solusi);
+		// for (int i = 1; i <= M.persamaan; i++){
+		// 	System.out.println("X["+i+"] = " + solusi[i]);
+		// }
+		// interpolasi();
 	}
 }
